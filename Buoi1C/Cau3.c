5214fd5e8f055eeca9b8a0122f99a17c507cd027
name <lequanganh6723@gmail.com>
2022-12-14 07:21:37 +0700

    Cau 3 buoi 1 C

--- Buoi1C/Cau3.c
+++ Buoi1C/Cau3.c
@@ -1,123 +1,150 @@
 #include<stdio.h>
 
-#define doomsday_year 1900
+typedef enum {
+    KhongNhuan, Nhuan
+}nam_Nhuan;
 
-int day = 2;
-int nam = 2022;
-int ngay = 15;
-int thang = 0;
-
-int tim_doomsday(int year)
+nam_Nhuan check_namNhuan(int nam)
 {
-    int a, b, c, d, e;
-    int kc = year - doomsday_year;
-    a = kc / 12;
-    b = kc % 12;
-    c = b / 4;
-    d = a + b + c;
-    e = d % 7;
-    // printf(" %d \r\n", e);
-    if (e > 4) {
-        e = e - 4;
-        // printf(" %d \r\n", e);
-        day = e - 1;
-        // printf(" %d \r\n", day);
-    }
-    else
+    if((nam % 4 == 0 && nam % 100 != 0) || (nam % 400 == 0))
     {
-        day = day + e;
-    } 
-    return day;
-}
-
-void print_day(int year)
-{
-    printf("\r\n\t\t Doomsday cua nam %d la ", year);
-    switch (day)
-    {
-    case 0:
-        printf("Thu 2\r\n");
-        break;
-    case 1:
-        printf("Thu 3\r\n");
-        break;
-    case 2:
-        printf("Thu 4\r\n");
-        break;
-    case 3:
-        printf("Thu 5\r\n");
-        break;
-    case 4:
-        printf("Thu 6\r\n");
-        break;
-    case 5:
-        printf("Thu 7\r\n");
-        break;
-    case 6:
-        printf("Chu Nhat\r\n");
-        break;
-
-    default:
-        break;
+        return Nhuan;
     }
+    return KhongNhuan;
 }
-
-void tim_ngay()
-{
-    switch (thang)
-    {
-    case 1:
-
-        break;
-    case 2:
-        
-        break;
-    case 3:
-        
-        break;
-    case 4:
-        
-        break;
-    case 5:
-        
-        break;
-    case 6:
-        
-        break;
-    case 7:
-        
-        break;
-    case 8:
-        
-        break;
-    case 9:
-        
-        break;
-    case 10:
-        
-        break;
-    case 11:
-        
-        break;
-    case 12:
-        
-        break;
-    default:
-        break;
-    }
-
-}
-
 
 int main()
 {
-    
-    print_day(nam);
-    check_ngay(4);
-    
-    
-    return 0;
+    int ngay, thang, nam;
+    printf("Nhap Ngay: ");
+    scanf("%d", &ngay);
+    printf("Nhap Thang: ");
+    scanf("%d", &thang);
+    printf("Nhap Nam: ");
+    scanf("%d", &nam);
 
+    check_namNhuan(nam);
+    int temp = nam / 100;
+
+    int AD;
+    switch (temp)
+    {
+    case 0:
+        AD = 3;
+        break;
+    case 1:
+        AD = 0;
+    case 2:
+        AD = 6;
+    case 3:
+        AD = 4;
+    default:
+        break;
+    }
+
+    int a, b, c, s, t, thu;
+    temp = nam % 100;
+    a = temp / 12;
+    b = temp % 12;
+    c = b / 4;
+    s = a + b + c;
+    t = s % 7;
+    thu = t + AD;
+
+    int MD;
+    switch (thang)
+    {
+    case 1:
+        if(check_namNhuan(nam) == Nhuan)
+        {
+            MD = 4;
+        }
+        else 
+        {
+            MD = 3;
+        }
+        break;
+    case 2:
+        if(check_namNhuan(nam) == Nhuan)
+        {
+            MD = 29;
+        }
+        else 
+        {
+            MD = 28;
+        }
+        break;
+    case 3:
+        MD = 0;
+        break;
+    case 4:
+        MD = 4;
+        break;
+    case 5:
+        MD = 9;
+        break;
+    case 6:
+        MD = 6;
+        break;
+    case 7:
+        MD = 11;
+        break;
+    case 8:
+        MD = 8;
+        break;
+    case 9:
+        MD = 5;
+        break;
+    case 10:
+        MD = 10;
+        break;
+    case 11:
+        MD = 7;
+        break;
+    case 12:
+        MD = 12;
+        break;
+    default:
+        break;
+    }
+
+    thu = thu + ngay - MD;
+    while (thu < 0)
+    {
+        thu = thu + 7;
+    }
+    thu = thu % 7 - 1;
+
+    switch (thu)
+    {
+    case 0:
+        printf("Ngay %d / %d / %d la Thu 7.\r\n", ngay, thang, nam);
+        break;
+    case 1:
+        printf("Ngay %d / %d / %d la Chu nhat.\r\n", ngay, thang, nam);
+        break;
+    case 2:
+        printf("Ngay %d / %d / %d la Thu 2.\r\n", ngay, thang, nam);
+        break;
+    case 3:
+        printf("Ngay %d / %d / %d la Thu 3.\r\n", ngay, thang, nam);
+        break;
+    case 4:
+        printf("Ngay %d / %d / %d la Thu 4.\r\n", ngay, thang, nam);
+        break;
+    case 5:
+        printf("Ngay %d / %d / %d la Thu 5.\r\n", ngay, thang, nam);
+        break;
+    case 6:
+        printf("Ngay %d / %d / %d la Thu 6.\r\n", ngay, thang, nam);
+        break;
+    default:
+        break;
+    }
+
+
+    return 0;
 }
+
 
 
